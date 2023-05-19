0903ea7a7d63afd23217307d02a989f887704d25
robiul0278 <robiul0278@gmail.com>
2023-05-19 19:36:00 +0600

    delete method complete

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({}));
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.djxbtyf.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,90 +25,98 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
     // Shop by category ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-    const sportsCollection = client.db('SuperCar').collection("sportsCars");
-    const trucksCollection = client.db('SuperCar').collection("trucks");
-    const policeCollection = client.db('SuperCar').collection("police");
-    const toysCollection = client.db('SuperCar').collection("allToys");
+    const sportsCollection = client.db("SuperCar").collection("sportsCars");
+    const trucksCollection = client.db("SuperCar").collection("trucks");
+    const policeCollection = client.db("SuperCar").collection("police");
+    const toysCollection = client.db("SuperCar").collection("allToys");
 
     // get data sports-car
-    app.get('/sports', async (req, res) => {
+    app.get("/sports", async (req, res) => {
       const result = await sportsCollection.find({}).toArray();
       res.send(result);
-    })
+    });
     // get data truck
-    app.get('/trucks', async (req, res) => {
+    app.get("/trucks", async (req, res) => {
       const result = await trucksCollection.find({}).toArray();
       res.send(result);
-    })
+    });
     // get data police-car
-    app.get('/police', async (req, res) => {
+    app.get("/police", async (req, res) => {
       const result = await policeCollection.find({}).toArray();
       res.send(result);
-    })
+    });
 
-    // find one 
+    // find one
     app.get("/sports/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId (id)}
+      const query = { _id: new ObjectId(id) };
       const data = await sportsCollection.findOne(query);
       res.send(data);
-    })
-    // find one 
+    });
+    // find one
     app.get("/trucks/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId (id)}
+      const query = { _id: new ObjectId(id) };
       const data = await trucksCollection.findOne(query);
       res.send(data);
-    })
-    // find one 
+    });
+    // find one
     app.get("/police/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId (id)}
+      const query = { _id: new ObjectId(id) };
       const data = await policeCollection.findOne(query);
       res.send(data);
-    })
+    });
     // Shop by category ||||||||||||||||||||||||||||||||||||||||||||| END--
 
-
     // Add A Toy |||||||||||||||||||||||||||||||||||||||||||||||||||| START--
-    app.post('/toys', async (req, res) => {
+    app.post("/toys", async (req, res) => {
       const data = req.body;
       const result = await toysCollection.insertOne(data);
       res.send(result);
-      console.log('new product', data)
-    })
+      console.log("new product", data);
+    });
 
-    // get data all toys 
-    app.get('/toys', async (req, res) => {
+    // get data all toys
+    app.get("/toys", async (req, res) => {
       const result = await toysCollection.find({}).toArray();
       res.send(result);
-    })
+    });
 
-    // get some user update toy data 
-    app.get('/toys', async (req, res) => {
-      let query = {}
-      if(req.query?.email) {
-        query = {email: req.query.email}
+    // get some user update toy data
+    app.get("/toys", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
       const result = await toysCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
-    // delete toys 
-    app.delete('/toys/:id', async (req, res) => {
+    // find one
+    app.get("/toys/:id", async (req, res) => {
       const id = req.params.id;
-      console.log('delete id', id);
-      const query = {_id: new ObjectId (id)}
+      const query = { _id: new ObjectId(id) };
+      const data = await toysCollection.findOne(query);
+      res.send(data);
+    });
+
+    // delete toys
+    app.delete("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("delete id", id);
+      const query = { _id: new ObjectId(id) };
       const result = await toysCollection.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -119,13 +124,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("SuperCar is running...!");
+});
 
-
-
-app.get('/', (req, res) => {
-    res.send('SuperCar is running...!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
